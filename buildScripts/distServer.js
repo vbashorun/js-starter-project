c344e9d524d6d89af66c507104c9ea86b216a4b2
Victor Bashorun <victor_m_bashorun@homedepot.com>
2019-10-15 16:54:31 -0400

    Development-ready projects sans logging.

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -1,25 +1,21 @@
+/*eslint-disable no-console */
 import express from 'express';
 import path from 'path';
 import open from 'open';
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
+import compression from 'compression';
 
-const port = 3000;
+const port = 3001;
 const app = express();
-const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+app.use(compression()); // enables G-zip compression
+app.use(express.static('dist'));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
-
   res.json([
     { "id": 1, "firstName": "Bob", "lastName":"Smith", "email":"bob@gmail.com"},
     { "id": 2, "firstName": "Tammy", "lastName":"Norton", "email":"tnorton@yahoo.com"},
--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () => {
-  it('should say hello', (done) => {
+  it('should have header that says Homies', (done) => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("What's good, fam?");
+      expect(h1.innerHTML).to.equal("Homies");
       done();
       window.close();
     });
